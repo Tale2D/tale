4403b67cca9c47607b414aeefe25ec5a610c6579
tale2d <120962789@qq.com>
2021-01-19 22:14:25 +0800

    init

---
+++ samples/Test/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Tale.Core;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var core = new Core();
+            //Console.WriteLine("Hello World!");
+            core.CWindows();
+        }
+    }
+}
